a0c0883e62a32168a8c54c60e2ffadfe3f10d8a1
Bradford M. Knowlton Jr <bradmkjr@BradfordsAirM1.fios-router.home>
2022-03-08 16:03:37 -0500

    changes classes to courses

--- src/index.php
+++ src/index.php
@@ -70,27 +70,27 @@
  *
  * @apiParam {Number} id Event ID.
  * 
- * @apiSuccess	(Success 201)	{json}	courses	All courses for event
+ * @apiSuccess	(Success 200)	{json}	courses	All courses for event
  * 
  * @apiSuccessExample	{json}	Success:
  * HTTP/1.1 200 OK
- *   {  
- *     "courses": [
- *       {
- *          "id": "123456",
- *          "sku": "22COrlando-LE22C278044",
- *          "code": "22COrlando-LE22C278044",
- *          "cpe_category_1": "",
- *          "cpe_credits_1": "3",
- *          "cpe_category_2": "",
- *          "cpe_credits_2": "3",
- *          "name": "Everybody's Critical Individual Tax Issues in 2022",
- *          "date": "June 13, 2022",
- *          "start_time": "07:30",
- *          "end_time": "13:00"
- *       }
- *     ]
- *	 }  
+ *  {  
+ *    "courses": [
+ *      {
+ *        "id": "123456",
+ *        "sku": "22COrlando-LE22C278044",
+ *        "code": "22COrlando-LE22C278044",
+ *        "cpe_category_1": "",
+ *        "cpe_credits_1": "3",
+ *        "cpe_category_2": "",
+ *        "cpe_credits_2": "3",
+ *        "name": "Everybody's Critical Individual Tax Issues in 2022",
+ *        "date": "June 13, 2022",
+ *        "start_time": "07:30",
+ *        "end_time": "13:00"
+ *     }
+ *   ]
+ * }  
  * 
  * @apiError	(Error 400)		{json}		Bad-Request				Invalid payload
  * @apiError	(Error 404)		{json}		Not-Found				Not Found
@@ -135,12 +135,13 @@
  *
  * @apiParam {Number} id Course ID.
  * 
- * @apiSuccess	(Success 201)	{json}	course	Course details with instructor
+ * @apiSuccess	(Success 200)	{json}	course	Course details with instructor
  * 
  * @apiSuccessExample	{json}	Success:
- * HTTP/1.1 200 OK
- *   {  
- *     "code": "22COrlando-LE22C278044 (actual code, not sku)",
+ *  HTTP/1.1 200 OK
+ *  {  
+ *     "id": "123456",
+ *     "code": "22COrlando-LE22C278044",
  *     "name": "Everybody's Critical Individual Tax Issues in 2022",
  *     "date": "June 13, 2022",
  *     "start_time": "07:30",
@@ -157,12 +158,12 @@
  *     "materials_5_file": "s3 url",
  *     "materials_6_name": "Sharons Blue Footed Boobies",
  *     "materials_6_file": "s3 url",
- *     "instructor_name: Mark Seid, Ea, CPA, USTCP",
+ *     "instructor_name": "Mark Seid, Ea, CPA, USTCP",
  *     "organization": "Western CPE",
  *     "organization_link": "westerncpe.com",
  *     "image": "https://www.westerncpe.com/wp-content/uploads/2020/01/WCPE-WebsiteBio-Mark-Seid.jpg",
  *     "bio": "Mark F. Seid, EA, CPA has an active tax practice in Paso Robles, California specializing in small businesses and tax controversy. A National Tax Practice Institute graduate, Mark is admitted to practice before the U.S. Tax Court. He has served as an Internal Revenue Agent with the IRS in San Jose and San Luis Obispo, California, a state director for the California Society of Enrolled Agents, and the chair for the society’s Finance and Budget committee. He regularly presents courses to tax professionals on issues affecting small businesses"		
- *	 }  
+ *	}  
  * 
  * @apiError	(Error 400)		{json}		Bad-Request				Invalid payload
  * @apiError	(Error 404)		{json}		Not-Found				Not Found
@@ -215,59 +216,18 @@
  * @apiGroup Courses
  * @apiDescription Record attendence for a user for a live event course
  *
+ * @apiParam {Number} id Course ID.
+ * @apiParam {Number} id User ID.
+ * 
  * @apiSuccess	(Success 201)	{json}	attendence	Record of live event attendance
  * 
  * @apiSuccessExample	{json}	Success:
- * HTTP/1.1 200 OK
+ * HTTP/1.1 201 OK
  *  {
  *    "course_id": "1234567",
  *    "user_id": "2124232",
  *    "recorded_at": "June 8th, 2022 12:34pm"
  *  }
- * 
- * @apiError	(Error 400)		{json}		Bad-Request				Invalid payload
- * @apiError	(Error 404)		{json}		Not-Found				Not Found
- * @apiError	(Error 409)		{json}		Conflict				requestItemID already exists
- * 
- * @apiErrorExample		{json}	Bad-Request:
- *	HTTP/1.1 400 Bad Request
- *	{
- *		"error": "Bad Request"
- *	}
- * @apiErrorExample		{json}	Invalid-accessKey:
- *	HTTP/1.1 401 Unauthorized
- *	{
- *		"error": "Invalid accessKey"
- *	}
- * @apiErrorExample		{json}	Not-Found:
- *	HTTP/1.1 404 Not Found
- *	{
- *		"error": "Not Found"
- *	}
- * @apiErrorExample		{json}	requestItemID-Already-Exists:
- *	HTTP/1.1 409 Conflict
- *	{
- *		"error": "requestItemID already exists"
- *	}
- */
-
-
-/**
- * @api {get} /products/feed.csv Request Product Feeds in CSV format
- * @apiVersion 2022.3.4
- * @apiName GetProductsCSV
- * @apiGroup Products
- * @apiDescription First view, query live events, return base details (all events listed on this page) One used for example
- *
- * @apiSuccess {String} display_name Display name of the User.
- *
- * @apiSuccess	(Success 201)	{String}	CSV	CSV of all available Products
- * 
- * @apiSuccessExample	{csv}	Success:
- *	HTTP/1.1 200 OK
- *	{
- *		"serviceOrderNumber": "1234567",
- *	}
  * 
  * @apiError	(Error 400)		{json}		Bad-Request				Invalid payload
  * @apiError	(Error 404)		{json}		Not-Found				Not Found
